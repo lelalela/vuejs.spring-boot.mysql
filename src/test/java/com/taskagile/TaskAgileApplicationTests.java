fbdacf43cfeef4afb055556a6a8af51aec4bfbfc
lelalela <lelalela@daum.net>
2020-10-26 11:18:46 +0900

    create back-end

---
+++ src/test/java/com/taskagile/TaskAgileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskagile;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAgileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
